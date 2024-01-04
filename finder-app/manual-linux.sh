3353031baefe14be890381712206888a8018b370
tkimweston <tkim_weston@yahoo.com>
2024-01-04 17:15:06 +0000

    fixed cp kernel

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -47,7 +47,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 fi
 
 echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}/rootfs
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
